e8d591999e56be8c47331566bc6ca643253a29f0
Arian N. Yamchelo <ariannajafi@outlook.com>
2023-05-13 20:17:59 +1000

    Revert "Add content"
    This reverts commit f2798c9c4651037ba81f71df84b3e5729a063e8b.

--- js/responsivenav.js
+++ js/responsivenav.js
@@ -2,15 +2,21 @@
 function addResponsiveClass() {
     let nav = document.getElementById("navMenu");
     let barsIcon = document.getElementById("barsIcon");
+    let closeIcon = document.getElementById("closeIcon");
 
     if(nav.classList.contains("responsive")){
         nav.classList.remove("responsive");
+        barsIcon.style.display = "block";
+        closeIcon.style.display = "none";
     } else {
         nav.classList.add("responsive");
+        barsIcon.style.display = "none";
+        closeIcon.style.display = "block";
     }
 }
 
 function removeResponsiveClass(){
     document.getElementById("navMenu").classList.remove("responsive");
     document.getElementById("barsIcon").style.display = "block";
+    document.getElementById("closeIcon").style.display = "none";
 }
